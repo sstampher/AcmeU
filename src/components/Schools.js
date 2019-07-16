e465f7606c5434016d384239edd315e8ae305a52
sstampher <stephanierstampher@gmail.com>
2019-07-15 19:15:18 -0700

    made student count work

--- src/components/Schools.js
+++ src/components/Schools.js
@@ -6,12 +6,24 @@ function Schools ( props ) {
         
         return <div>
                     < AddStudent />
-                    {props.schools.map( item => <li key={item.id}>{item.name}</li>)}
+                    {props.schools.map( item => <li key={item.id}>{item.name + ' ' + 'student count: ' + props.studentCount(item.name)}</li>)}
                </div>
     }
 
 const mapStateToProps = state => ({
-    schools: state.data.schools
+    schools: state.data.schools,
+    students: state.data.students,
+    studentCount: function(schoolName) {
+        let targetSchool = state.data.schools.find(school => school.name === schoolName)
+        let count = state.data.students.reduce( (acc, item) => {
+            if(item.schoolId === targetSchool.id){
+                console.log('yes');
+                acc += 1;
+            }
+            return acc;
+        }  , 0 )
+        return count;
+    }
 })
 
 export default connect( mapStateToProps )(Schools)
