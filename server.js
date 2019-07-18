ef07997877a80be3c6ef1bd52365c24494551367
sstampher <stephanierstampher@gmail.com>
2019-07-18 13:46:49 -0700

    final commit

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => `Server running on port ${port}`);
